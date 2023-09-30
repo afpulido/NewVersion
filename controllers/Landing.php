ec854b3c555601a4919b8cf439772ec889a9de85
Andres Pulido <afpulido509@hotmail.com>
2023-09-30 12:33:14 -0500

    02.Boot files

---
+++ controllers/Landing.php
@@ -0,0 +1,12 @@
+<?php 
+// Clase principal que arranca el código
+class Landing{
+    public function __construct(){}
+    public function main(){
+        echo "Estoy en el controlador Landing y su funcion principal";
+        /* require_once "views/company/header.view.php";
+        require_once "views/company/index.view.php";
+        require_once "views/company/footer.view.php"; */
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-asp
+<?php
+// Valida con la superglobal $_REQUEST que la variable 'c' no tenga ningún valor asignado por metodo get    
+if(!isset($_REQUEST['c'])){
+    //Se incluye y ejecuta el archivo Landing.php dentro de la carpeta controllers    
+    require_once "controllers/Landing.php";
+    // Se crea un objeto de la clase Landing y se ejecuta su metodo main()
+    $controller = new Landing;
+    $controller->main();
+}else{
+    // si la variable 'c' tiene un valor se le asigna a la variable global $controller
+    $controller = $_REQUEST['c'];
+    // se incluye y ejecuta el archivo concatenando el valor recibido por $controller
+    require_once "controllers/".$controller."php";
+    // crea una instancia de la clase que se especifica en la variable $controller
+    $controller = new $controller;
+    // Valida que la variable 'a' tenga un valor si lo tiene lo busca y lo trae sino asigna main
+    $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
+    // llama la funcion y la usa con el dato capturado anteriormente
+    call_user_func(array($controller,$action));
+}
+
+
+?>
